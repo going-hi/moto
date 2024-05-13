7f9730533e8c8a961a994c28c96b0dce08188bfd
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-05-13 20:39:15 +0300

    main.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { EnvConfigOptions } from './configs'
-import { DatabaseModule } from './core/database/database.module'
+import { EnvConfigOptions } from '@/configs'
+import { DatabaseModule } from '@/core/database/database.module'
+import { AuthModule } from '@/auth/auth.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(EnvConfigOptions), DatabaseModule]
+	imports: [ConfigModule.forRoot(EnvConfigOptions), DatabaseModule, AuthModule]
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { Logger } from '@nestjs/common'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	app.use(cookieParser())
+	app.setGlobalPrefix('api')
 
 	const configService = app.get(ConfigService)
 	const PORT = configService.get('PORT')
+	const CLIENT_URL = configService.get('CLIENT_URL')
+
+	app.enableCors({
+		origin: CLIENT_URL,
+		credentials: true
+	})
+
 	const logger = new Logger()
 	await app.listen(PORT)
 	logger.log(`Server started on port: ${PORT}`)
