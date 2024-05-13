ed1544fb3ad7085f0a6a7aec66a16851a8a99889
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-05-13 17:44:12 +0300

    database module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { EnvConfigOptions } from './configs'
+import { DatabaseModule } from './core/database/database.module'
 
 @Module({
-	imports: [],
-	providers: []
+	imports: [ConfigModule.forRoot(EnvConfigOptions), DatabaseModule]
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+
+	const configService = app.get(ConfigService)
+	const PORT = configService.get('PORT')
+	const logger = new Logger()
+	await app.listen(PORT)
+	logger.log(`Server started on port: ${PORT}`)
 }
 bootstrap()
