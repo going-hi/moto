ee101a61fd89c4bcf8a15e7da9198fb80140b9b8
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-05-15 19:53:44 +0300

    update mailService

--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -11,15 +11,18 @@ export class MailService {
 	) {}
 
 	async sendActiveLink(to: string, link: string) {
+		const API_URL = this.configService.get('API_URL')
+		const PORT = this.configService.get('PORT')
 		try {
 			await this.mailerService.sendMail({
 				to,
+				from: this.configService.get('SMTP_USER'),
 				subject: 'Подтверждение почты',
 				text: '',
 				html: `
                     <div>
-                    <h1>Ваша ссылка активации</h1>
-                    <h1>${link}</h1>
+                    <h1>Ваша ссылка активации для онлайн магазина Moto</h1>
+					<a href="${API_URL}:${PORT}/api/auth/active/${link}">Нажмите на это ссылку, чтобы подтвердить почту</a>
                     </div>
                 `
 			})
