250fc2411698e5af94dffe49df49b2e86c58d172
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-05-18 00:52:33 +0300

    product dtos

--- server/src/modules/product/dto/index.ts
+++ server/src/modules/product/dto/index.ts
@@ -1,3 +1,4 @@
 export * from './create-product.dto'
 export * from './update-product.dto'
 export * from './product-all.query.dto'
+export * from './image.dto'
---
+++ server/src/modules/product/dto/product-all.query.dto.ts
@@ -0,0 +1,3 @@
+import { PaginationQuery } from '@/common/pagination'
+
+export class ProductAllQueryDto extends PaginationQuery {}
