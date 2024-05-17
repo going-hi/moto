0aa237030fe6d23c75145e48c34d11a34482225e
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-05-18 00:55:59 +0300

    product module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,8 +4,20 @@ import { EnvConfigOptions } from '@/configs'
 import { DatabaseModule } from '@/core/database/database.module'
 import { AuthModule } from '@/auth/auth.module'
 import { MailModule } from '@/mail/mail.module'
+import { FileModule } from '@/core/file/file.module'
+import { ProductModule } from './modules/product/product.module'
+import { CharacteristicModule } from './modules/characteristic/characteristic.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(EnvConfigOptions), DatabaseModule, AuthModule, MailModule]
+	imports: [
+		ConfigModule.forRoot(EnvConfigOptions),
+		DatabaseModule,
+		AuthModule,
+		MailModule,
+		FileModule,
+		// * @/modules
+		ProductModule,
+		CharacteristicModule
+	]
 })
 export class AppModule {}
---
+++ server/src/modules/product/product.service.ts
@@ -0,0 +1,117 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { ProductEntity } from './entities'
+import { Repository } from 'typeorm'
+import { CreateProductDto, ImageDto, ProductAllQueryDto, UpdateProductDto } from './dto'
+import { FileService } from '@/core/file/file.service'
+import { CharacteristicEntity } from '../characteristic/entities'
+import { skipCount } from '@/core/utils'
+import { PaginationDto } from '@/common/pagination'
+
+@Injectable()
+export class ProductService {
+	constructor(
+		@InjectRepository(ProductEntity)
+		private readonly productRepository: Repository<ProductEntity>,
+		private readonly fileService: FileService
+	) {}
+
+	async create(dto: CreateProductDto, images: Express.Multer.File[]) {
+		const orderUrls = images.map((image, index) => ({ image, index }))
+
+		const albumsObj = await Promise.all(
+			orderUrls.map(async imgObj => {
+				return {
+					index: imgObj.index,
+					url: await this.fileService.uploadFile(imgObj.image)
+				}
+			})
+		)
+		const albums = albumsObj.sort((a, b) => a.index - b.index).map(imgObj => imgObj.url)
+
+		let chars = []
+		if (dto.characteristics?.length) {
+			chars = dto.characteristics.map(char => CharacteristicEntity.of(char))
+		}
+		const product = this.productRepository.create({
+			...dto,
+			images: albums,
+			characteristics: chars
+		})
+		return await this.productRepository.save(product)
+	}
+
+	async getOne(id: number) {
+		const product = await this.productRepository.findOne({
+			where: { id },
+			relations: {
+				characteristics: true
+			}
+		})
+
+		if (!product) throw new NotFoundException(`Товар с id: ${id} не найден`)
+		return product
+	}
+	async getAll({ count, page }: ProductAllQueryDto) {
+		const where = {}
+		const [products, total] = await this.productRepository.findAndCount({
+			order: {},
+			where,
+			take: count,
+			skip: skipCount(page, count)
+		})
+		return new PaginationDto(products, total)
+	}
+
+	async update(id: number, dto: UpdateProductDto) {
+		const product = await this.byId(id, true)
+		return await this.productRepository.save({ ...product, ...dto })
+	}
+
+	async deleteImages(id: number, dto: ImageDto) {
+		const product = await this.byId(id, true)
+		dto.images.forEach(url => {
+			if (!product.images.includes(url)) {
+				throw new BadRequestException(`Файл ${url} в товаре с id: ${id} не найден`)
+			}
+			product.images = product.images.filter(img => img !== url)
+		})
+
+		await this.productRepository.save(product)
+		Promise.all(dto.images.map(async url => await this.fileService.deleteFile(url)))
+		return
+	}
+
+	async addImages(id: number, images: Express.Multer.File[]) {
+		const product = await this.byId(id, true)
+		const orderUrls = images.map((image, index) => ({ image, index }))
+
+		const albumsObj = await Promise.all(
+			orderUrls.map(async imgObj => {
+				return {
+					index: imgObj.index,
+					url: await this.fileService.uploadFile(imgObj.image)
+				}
+			})
+		)
+		const albums = albumsObj.sort((a, b) => a.index - b.index).map(imgObj => imgObj.url)
+
+		product.images = [...product.images, ...albums]
+		return await this.productRepository.save(product)
+	}
+
+	async delete(id: number) {
+		await this.byId(id, true)
+		await this.productRepository.delete({ id })
+		return
+	}
+
+	async byId(id: number, withError = false) {
+		const product = await this.productRepository.findOneBy({ id })
+
+		if (!product && withError) {
+			throw new NotFoundException(`Товар с id: ${id} не найден`)
+		}
+		return product
+	}
+}
