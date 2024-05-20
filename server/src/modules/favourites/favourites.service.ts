b66258b9527e202442909d264be04f3bd6f279a2
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-05-20 21:02:18 +0300

    favourites module

---
+++ server/src/modules/favourites/favourites.service.ts
@@ -0,0 +1,111 @@
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common'
+import { CreateFavouritesDto, FavouritesAllQueryDto } from './dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { FavouritesEntity } from './entities'
+import { Repository } from 'typeorm'
+import { ProductService } from '../product/product.service'
+import { skipCount } from '@/core/utils'
+import { PaginationDto } from '@/common/pagination'
+
+@Injectable()
+export class FavouritesService {
+	constructor(
+		@InjectRepository(FavouritesEntity)
+		private readonly favouritesRepository: Repository<FavouritesEntity>,
+		private readonly productService: ProductService
+	) {}
+
+	async create(userId: number, { product }: CreateFavouritesDto) {
+		await this.productService.byId(product, true)
+		const dto = {
+			product: {
+				id: product
+			},
+			user: {
+				id: userId
+			}
+		}
+		const oldFavourites = await this.favouritesRepository.findOne({
+			where: dto,
+			select: {
+				product: {
+					id: true
+				},
+				user: {
+					id: true
+				}
+			},
+			relations: {
+				user: true,
+				product: true
+			}
+		})
+		if (oldFavourites) return oldFavourites
+		const favourites = this.favouritesRepository.create(dto)
+		return await this.favouritesRepository.save(favourites)
+	}
+
+	// TODO: обсудить поля
+	async findAll({ count, page, sortBy, sortOrder, user }: FavouritesAllQueryDto) {
+		const where = {}
+		user ? (where['user'] = { id: user }) : {}
+		const [items, total] = await this.favouritesRepository.findAndCount({
+			where,
+			order: {
+				[sortBy]: sortOrder
+			},
+			take: count,
+			skip: skipCount(page, count),
+			relations: {
+				user: true,
+				product: true
+			},
+			select: {
+				user: {
+					id: true,
+					email: true,
+					name: true,
+					role: true
+				}
+			}
+		})
+
+		return new PaginationDto(items, total)
+	}
+
+	async findOne(id: number, userId?: number) {
+		const where = { id }
+
+		userId ? (where['user'] = { id: userId }) : {}
+
+		const favourites = await this.favouritesRepository.findOne({
+			where,
+			relations: {
+				user: true,
+				product: true
+			},
+			select: {
+				user: {
+					id: true,
+					email: true,
+					name: true,
+					role: true
+				}
+			}
+		})
+		if (!favourites) throw new NotFoundException(`Избранное с id: ${id} не найдена`)
+		return favourites
+	}
+
+	async delete(id: number, userId?: number) {
+		if (userId) await this.checkByUser(id, userId)
+		await this.favouritesRepository.delete({ id })
+		return
+	}
+
+	async checkByUser(id: number, userId: number) {
+		const favourites = await this.findOne(id)
+		if (favourites.user.id !== userId) throw new ForbiddenException()
+		return favourites
+	}
+}
