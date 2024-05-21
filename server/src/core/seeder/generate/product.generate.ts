c28195a2d772c2cebf6d5134bd708085e7bd9557
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-05-21 23:39:22 +0300

    add random enum

--- server/src/core/seeder/generate/product.generate.ts
+++ server/src/core/seeder/generate/product.generate.ts
@@ -1,16 +1,17 @@
 import { CategoriesToType } from '@/common/dto'
 import { ECategory } from '@/common/enums'
+import { getRandomEnumValue } from '@/core/utils'
 import { ProductEntity } from '@/modules/product/entities'
 import { faker } from '@faker-js/faker'
 
 export function productGenerate() {
-	const category = ECategory.MOTORCYCLES
+	const category = getRandomEnumValue(ECategory)
 	const product = new ProductEntity()
 	product.name = faker.commerce.productName()
 	product.price = +faker.commerce.price({ dec: 0 })
 	product.description = faker.commerce.productDescription()
 	product.category = category
-	product.type = CategoriesToType[category].MOTORCYCLES
+	product.type = getRandomEnumValue(CategoriesToType[category]) || 'other'
 	product.brand = faker.company.name()
 	product.images = ['image/123123.png', 'image/123123.png', 'image/123123.png']
 	return product
