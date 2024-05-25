e474dbdf4b2341214519d4e7d5170eb9fd5f04a9
coldnanee <pmihaylov036@gmail.com>
2024-05-25 16:16:19 +0300

    fix bugs

--- client/src/shared/ui/button/@primary/primary.tsx
+++ client/src/shared/ui/button/@primary/primary.tsx
@@ -4,7 +4,7 @@ export const Primary = ({ children, ...props }: { children: ReactNode }) => {
 	return (
 		<button
 			{...props}
-			className='py-[23px] w-full bg-black text-beige font-bold'
+			className='py-[23px] w-full bg-black text-beige font-bold dhover:hover:scale-[102%] duration-700'
 		>
 			{children}
 		</button>
