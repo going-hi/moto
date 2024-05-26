6fc7a14a113c19b062eb6d0c18d817c6b5d588e3
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-05-26 13:59:28 +0300

    user module base

--- server/src/modules/user/user.module.ts
+++ server/src/modules/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service'
 import { UserController } from './user.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserEntity } from './entities'
+import { FileModule } from '@/core/file/file.module'
 
 @Module({
-	imports: [TypeOrmModule.forFeature([UserEntity])],
+	imports: [TypeOrmModule.forFeature([UserEntity]), FileModule],
 	controllers: [UserController],
 	providers: [UserService],
 	exports: [UserService]
