29afa5216ee0edf5ea9a5e4bba05692357e0a15e
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-05-27 18:31:20 +0300

    update favourites dto

--- server/src/modules/favourites/dto/favourites-all.query.dto.ts
+++ server/src/modules/favourites/dto/favourites-all.query.dto.ts
@@ -1,14 +1,13 @@
 import { ESortOrder } from '@/common/enums'
-import { PaginationQuery } from '@/common/pagination'
+import { PaginationUserQuery } from '@/common/pagination'
 import { ApiProperty, OmitType } from '@nestjs/swagger'
-import { Type } from 'class-transformer'
-import { IsEnum, IsInt, IsOptional } from 'class-validator'
+import { IsEnum } from 'class-validator'
 
 enum ESort {
 	CREATE_DATE = 'createDate'
 }
 
-export class FavouritesAllQueryDto extends PaginationQuery {
+export class FavouritesAllQueryDto extends PaginationUserQuery {
 	@ApiProperty({ enum: ESort })
 	@IsEnum(ESort)
 	sortBy: ESort = ESort.CREATE_DATE
@@ -16,12 +15,6 @@ export class FavouritesAllQueryDto extends PaginationQuery {
 	@ApiProperty({ enum: ESortOrder })
 	@IsEnum(ESortOrder)
 	sortOrder: ESortOrder = ESortOrder.ASC
-
-	@ApiProperty()
-	@IsOptional()
-	@IsInt({ message: 'Id Пользователя должно быть числом' })
-	@Type(() => Number)
-	user?: number
 }
 
-export class FavouritesAllQueryDtoForUser extends OmitType(FavouritesAllQueryDto, ['user']) {}
+export class FavouritesAllForUserQueryDto extends OmitType(FavouritesAllQueryDto, ['user']) {}
