fb3f107e1588655f03f6988a2d456bdfd4ea783d
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-06-04 20:23:36 +0300

    product generate

--- server/src/core/seeder/generate/product.generate.ts
+++ server/src/core/seeder/generate/product.generate.ts
@@ -1,8 +1,9 @@
-import { CategoriesToType } from '@/common/dto'
 import { ECategory } from '@/common/enums'
 import { getRandomEnumValue } from '@/core/utils'
 import { ProductEntity } from '@/modules/product/entities'
 import { faker } from '@faker-js/faker'
+import * as filterData from '@/modules/product/dto/filter.json'
+import { characteristicGenerateByFilter } from './characteristic.generate'
 
 export function productGenerate() {
 	const category = getRandomEnumValue(ECategory)
@@ -11,8 +12,22 @@ export function productGenerate() {
 	product.price = +faker.commerce.price({ dec: 0 })
 	product.description = faker.commerce.productDescription()
 	product.category = category
-	product.type = getRandomEnumValue(CategoriesToType[category]) || 'other'
+	let filter: object
+	let type: string | null
+
+	const filterCategory = filterData[category]
+	if (filterCategory?.common) {
+		type = null
+		filterCategory['char']
+		filter = filterCategory['char']
+	} else {
+		const typeArray = Object.keys(filterCategory)
+		type = typeArray[Math.floor(Math.random() * typeArray.length)]
+		filter = filterCategory[type]
+	}
 	product.brand = faker.company.name()
-	product.images = ['image/123123.png', 'image/123123.png', 'image/123123.png']
+	product.images = []
+	product.type = type
+	product.characteristics = characteristicGenerateByFilter(filter)
 	return product
 }
