bfbf8e5f710899944b06fbd9a4d84a68b507fc39
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-06-06 20:58:30 +0300

    update seeder

--- server/src/core/seeder/generate/characteristic.generate.ts
+++ server/src/core/seeder/generate/characteristic.generate.ts
@@ -1,9 +1,20 @@
 import { CharacteristicEntity } from '@/modules/characteristic/entities'
 
-export function characteristicGenerateByFilter(filter: object) {
+export function characteristicGenerateByFilter(filter: object, brand?: string) {
 	const characteristics = []
 	Object.keys(filter).forEach(key => {
 		if (key === 'цена') return
+
+		if (brand) {
+			if (key.toLowerCase() === 'бренд') {
+				const char = new CharacteristicEntity()
+				char.key = key
+				char.value = brand
+				characteristics.push(char)
+				return
+			}
+		}
+
 		const char = new CharacteristicEntity()
 		char.key = key
 		const value = filter[key][Math.floor(Math.random() * filter[key].length)]
